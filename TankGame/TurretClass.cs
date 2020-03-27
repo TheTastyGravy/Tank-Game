c7c8efdecda84cd7db1de4c463f847864aa4bcd3
Thomas-PC <=>
2020-03-27 17:09:39 +1100

    Added images and created turret script

---
+++ TankGame/TurretClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using rl = Raylib;
+using MthLib = MathLibrary;
+
+namespace TankGame
+{
+	class TurretClass
+	{
+
+
+
+
+
+	}
+}
